d7412acab60491918897d97a8bae9bcbe6ec6115
Jcarlos625 <jeancmt79@gmail.com>
2025-07-02 20:03:33 -0600

    Agregando javascrip

---
+++ Javascrip/sintaxis.js
@@ -0,0 +1,22 @@
+console.log("Hola, Mundo!");
+
+//variables y constantes
+let variable = "Soy una variable";
+// var variable2 = "Soy una variable 2";
+
+variable = "3"
+
+//concatenación
+console.log("hola "+variable);
+
+//operadores matematicos
+//operadores de comparación
+//operadores de comparacion estricta
+//operadores de comparacion numerica
+//operadores lógicos
+//estructuras de control y condicionales
+//ternario
+//estructuras de repetición o bucles (loops)
+
+
+
